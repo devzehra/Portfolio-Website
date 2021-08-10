76581ee8bc87e0574f3581147a23864ea2d81034
devzehra <zehranurozpolat@gmail.com>
2021-08-10 22:48:36 +0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-
 /* MENU SHOW */
 /* Validate if contacts exists */
 
@@ -32,7 +31,23 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*ACCORDION SKILLS*/
+const skillsContent = document.getElementsByClassName('.skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i= 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 /*QUALIFICATTON TABS*/
 
 /*SERVICES MODAL*/
